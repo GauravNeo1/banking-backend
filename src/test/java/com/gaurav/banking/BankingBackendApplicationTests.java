c857149aa6225f8204be400f14a54117d182389c
Gaurav Patil <patil.gaurav@neosoftmail.com>
2026-06-01 00:37:09 +0530

    Initial Spring Boot setup

---
+++ src/test/java/com/gaurav/banking/BankingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaurav.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
